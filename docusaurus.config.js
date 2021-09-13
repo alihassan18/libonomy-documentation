1b3fbb174336d92482f9941d1c5f015752b37431
jibran baig <jibran.baig@magnusmage.com>
2021-09-13 14:39:42 +0500

    meta tags added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,129 +3,129 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: "Libonomy Documentation | Libonomy Documentation",
-  tagline: "Documentation",
-  url: "https://libonomy.com/",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.png",
-  organizationName: "Libonomy", // Usually your GitHub org/user name.
-  projectName: "libonomy", // Usually your repo name.
-  themeConfig: {
-    colorMode: {
-      defaultMode: "light",
-      disableSwitch: false,
-      respectPrefersColorScheme: false,
-    },
-
-    navbar: {
-      // title: "LIBONOMY",
-      logo: {
-        alt: "Libonomy Logo",
-        src: "img/logo1.png",
-      },
-      items: [
-        {
-          type: "doc",
-          docId: "Introduction/high-level-overview",
-          position: "right",
-          label: "Quick Start",
+    title: "Libonomy Docs",
+    tagline: "Documentation",
+    url: "https://libonomy.com/",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.png",
+    organizationName: "Libonomy", // Usually your GitHub org/user name.
+    projectName: "libonomy", // Usually your repo name.
+    themeConfig: {
+        colorMode: {
+            defaultMode: "light",
+            disableSwitch: false,
+            respectPrefersColorScheme: false,
         },
-        // { to: "/blog", label: "Blog", position: "right" },
-        {
-          href: "https://github.com/libonomy",
-          label: "GitHub",
-          position: "right",
-        },
-      ],
-    },
-    footer: {
-      style: "light",
 
-      links: [
-        {
-          title: "Libonomy",
-          items: [
-            {
-              html: `
+        navbar: {
+            // title: "LIBONOMY",
+            logo: {
+                alt: "Libonomy Logo",
+                src: "img/logo1.png",
+            },
+            items: [
+                {
+                    type: "doc",
+                    docId: "Introduction/high-level-overview",
+                    position: "right",
+                    label: "Quick Start",
+                },
+                // { to: "/blog", label: "Blog", position: "right" },
+                {
+                    href: "https://github.com/libonomy",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
+        },
+        footer: {
+            style: "light",
+
+            links: [
+                {
+                    title: "Libonomy",
+                    items: [
+                        {
+                            html: `
                                 <p>Libonomy LTD | 483 Green Lanes, N13 4BS | London United Kingdom
                                 </br>info@libonomy.com</br>
                                 +44 742 458 6677
                                 </p>
                               `,
-            },
-          ],
-        },
-        {
-          title: "Docs",
-          items: [
-            {
-              label: "Quick Start",
-              to: "/docs/Introduction/high-level-overview",
-            },
-            {
-              label: "Installation",
-              to: "/docs/Guide/Installation/install-cusp",
-            },
-            {
-              label: "POS/DPOS Guide",
-              to: "/docs/Guide/Installation/join-staking-network",
-            },
-          ],
-        },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Discord",
-              href: "https://discord.com/invite/6P9SX5c",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/libonomy",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            // {
-            //     label: "Blog",
-            //     to: "/blog",
-            // },
-            {
-              label: "GitHub",
-              href: "https://github.com/libonomy",
-            },
-          ],
-        },
-      ],
+                        },
+                    ],
+                },
+                {
+                    title: "Docs",
+                    items: [
+                        {
+                            label: "Quick Start",
+                            to: "/docs/Introduction/high-level-overview",
+                        },
+                        {
+                            label: "Installation",
+                            to: "/docs/Guide/Installation/install-cusp",
+                        },
+                        {
+                            label: "POS/DPOS Guide",
+                            to: "/docs/Guide/Installation/join-staking-network",
+                        },
+                    ],
+                },
+                {
+                    title: "Community",
+                    items: [
+                        {
+                            label: "Discord",
+                            href: "https://discord.com/invite/6P9SX5c",
+                        },
+                        {
+                            label: "Twitter",
+                            href: "https://twitter.com/libonomy",
+                        },
+                    ],
+                },
+                {
+                    title: "More",
+                    items: [
+                        // {
+                        //     label: "Blog",
+                        //     to: "/blog",
+                        // },
+                        {
+                            label: "GitHub",
+                            href: "https://github.com/libonomy",
+                        },
+                    ],
+                },
+            ],
 
-      copyright: "© Libonomy (2021). All rights reserved.",
+            copyright: "© Libonomy (2021). All rights reserved.",
+        },
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+        },
     },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-  },
-  presets: [
-    [
-      "@docusaurus/preset-classic",
-      {
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl: "https://github.com/libonomy",
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      },
+    presets: [
+        [
+            "@docusaurus/preset-classic",
+            {
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    // Please change this to your repo.
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    editUrl: "https://github.com/libonomy",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            },
+        ],
     ],
-  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -23,190 +23,180 @@ import { Link } from "react-router-dom";
 //     );
 // }
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+    const { siteConfig } = useDocusaurusContext();
 
-  const { isDarkTheme, setLightTheme, setDarkTheme } = useThemeContext();
-  return (
-    <div className={clsx(styles.heroBanner)}>
-      <div className="container">
-        <div className={clsx(styles.bannerSection)}>
-          <div className={(clsx("col col-md-8"), styles.bannerContent)}>
-            <div className={styles.bannerContentWrapper}>
-              <div
-                className={isDarkTheme ? styles.titleDark : styles.titleLight}
-              >
-                Libonomy 
-              </div>
-              <div
-                className={
-                  isDarkTheme ? styles.descriptionDark : styles.descriptionLight
-                }
-              >
-                Interoperable, scalable and AI based blockchain technology that
-                allows communities and companies to create faster and more
-                secure decentralized cross chain solutions - digital asset
-                lending and borrowing, virtual storage, hedging and even
-                power-sharing between units 
-              </div>
-              <div
-                className={
-                  isDarkTheme ? styles.headingDark : styles.headingLight
-                }
-              >
-                Documentation
-              </div>
-              <div className={styles.end}>
-                <div
-                  className={
-                    isDarkTheme ? styles.subHeadingDark : styles.subHeadingLight
-                  }
-                >
-                  Quick Guide
+    const { isDarkTheme, setLightTheme, setDarkTheme } = useThemeContext();
+    return (
+        <div className={clsx(styles.heroBanner)}>
+            <div className="container">
+                <div className={clsx(styles.bannerSection)}>
+                    <div className={(clsx("col col-md-8"), styles.bannerContent)}>
+                        <div className={styles.bannerContentWrapper}>
+                            <div className={isDarkTheme ? styles.titleDark : styles.titleLight}>
+                                Libonomy
+                            </div>
+                            <div
+                                className={
+                                    isDarkTheme ? styles.descriptionDark : styles.descriptionLight
+                                }>
+                                Interoperable, scalable and AI based blockchain technology that
+                                allows communities and companies to create faster and more secure
+                                decentralized cross chain solutions - digital asset lending and
+                                borrowing, virtual storage, hedging and even power-sharing between
+                                units
+                            </div>
+                            <div className={isDarkTheme ? styles.headingDark : styles.headingLight}>
+                                Documentation
+                            </div>
+                            <div className={styles.end}>
+                                <div
+                                    className={
+                                        isDarkTheme ? styles.subHeadingDark : styles.subHeadingLight
+                                    }>
+                                    Quick Guide
+                                </div>
+                            </div>
+                            <div className={styles.btnContainer}>
+                                <div
+                                    className={
+                                        isDarkTheme ? styles.btnPrimayDark : styles.btnPrimayLight
+                                    }>
+                                    {isDarkTheme ? (
+                                        <Link to="/docs/Introduction/high-level-overview">
+                                            <img
+                                                src={
+                                                    require("../../static/img/bookDark.png").default
+                                                }
+                                                alt="image"
+                                                className={styles.iconPlay}
+                                            />
+                                        </Link>
+                                    ) : (
+                                        <Link to="/docs/Introduction/high-level-overview">
+                                            <img
+                                                src={
+                                                    require("../../static/img/bookLight.png")
+                                                        .default
+                                                }
+                                                alt="image"
+                                                className={styles.iconPlay}
+                                            />
+                                        </Link>
+                                    )}
+                                    SDK
+                                </div>
+                                <div
+                                    className={
+                                        isDarkTheme ? styles.btnPrimayDark : styles.btnPrimayLight
+                                    }>
+                                    {isDarkTheme ? (
+                                        <Link to="/">
+                                            <img
+                                                src={
+                                                    require("../../static/img/playDark.png").default
+                                                }
+                                                alt="image"
+                                                className={styles.iconPlay}
+                                            />
+                                        </Link>
+                                    ) : (
+                                        <Link to="/">
+                                            <img
+                                                src={
+                                                    require("../../static/img/playLight.png")
+                                                        .default
+                                                }
+                                                alt="image"
+                                                className={styles.iconPlay}
+                                            />
+                                        </Link>
+                                    )}
+                                    Tutorials (Soon)
+                                </div>
+                            </div>
+                        </div>
+                        <div className={clsx("col col--6")}>
+                            {isDarkTheme ? (
+                                <img
+                                    src={require("../../static/img/personDark.png").default}
+                                    alt="image"
+                                    className={styles.banImage}
+                                />
+                            ) : (
+                                <img
+                                    src={require("../../static/img/personLight.png").default}
+                                    alt="image"
+                                    className={styles.banImage}
+                                />
+                            )}
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className={styles.btnContainer}>
-                <div
-                  className={
-                    isDarkTheme ? styles.btnPrimayDark : styles.btnPrimayLight
-                  }
-                >
-                  {isDarkTheme ? (
-                    <Link to="/docs/Introduction/high-level-overview">
-                      <img
-                        src={require("../../static/img/bookDark.png").default}
-                        alt="image"
-                        className={styles.iconPlay}
-                      />
-                    </Link>
-                  ) : (
-                    <Link to="/docs/Introduction/high-level-overview">
-                      <img
-                        src={require("../../static/img/bookLight.png").default}
-                        alt="image"
-                        className={styles.iconPlay}
-                      />
-                    </Link>
-                  )}
-                  SDK
-                </div>
-                <div
-                  className={
-                    isDarkTheme ? styles.btnPrimayDark : styles.btnPrimayLight
-                  }
-                >
-                  {isDarkTheme ? (
-                    <Link to="/">
-                      <img
-                        src={require("../../static/img/playDark.png").default}
-                        alt="image"
-                        className={styles.iconPlay}
-                      />
-                    </Link>
-                  ) : (
-                    <Link to="/">
-                      <img
-                        src={require("../../static/img/playLight.png").default}
-                        alt="image"
-                        className={styles.iconPlay}
-                      />
-                    </Link>
-                  )}
-                  Tutorials (Soon)
-                </div>
-              </div>
             </div>
-            <div className={clsx("col col--6")}>
-              {isDarkTheme ? (
-                <img
-                  src={require("../../static/img/personDark.png").default}
-                  alt="image"
-                  className={styles.banImage}
-                />
-              ) : (
-                <img
-                  src={require("../../static/img/personLight.png").default}
-                  alt="image"
-                  className={styles.banImage}
-                />
-              )}
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 function SubscribeNow() {
-  const { siteConfig } = useDocusaurusContext();
-  const { isDarkTheme } = useThemeContext();
-  return (
-    <div
-      className={
-        isDarkTheme
-          ? clsx(styles.subscibeWrapperDark)
-          : clsx(styles.subscibeWrapperLight)
-      }
-    >
-      <div className="container">
-        <div className={styles.subscribeContent}>
-          <div>
-            <div
-              className={
-                isDarkTheme
-                  ? styles.subscribetitleDark
-                  : styles.subscribetitleLight
-              }
-            >
-              Subscribe now!
+    const { siteConfig } = useDocusaurusContext();
+    const { isDarkTheme } = useThemeContext();
+    return (
+        <div
+            className={
+                isDarkTheme ? clsx(styles.subscibeWrapperDark) : clsx(styles.subscibeWrapperLight)
+            }>
+            <div className="container">
+                <div className={styles.subscribeContent}>
+                    <div>
+                        <div
+                            className={
+                                isDarkTheme ? styles.subscribetitleDark : styles.subscribetitleLight
+                            }>
+                            Subscribe now!
+                        </div>
+                        <div
+                            className={
+                                isDarkTheme
+                                    ? styles.subscribeHeadingDark
+                                    : styles.subscribeHeadingLight
+                            }>
+                            Get our latest News and Docs in your INBOX.
+                        </div>
+                    </div>
+                    <div>
+                        <input
+                            placeholder="Enter email address"
+                            className={
+                                isDarkTheme ? styles.subscribeInputDark : styles.subscribeInputLight
+                            }
+                        />
+                        <button
+                            className={
+                                isDarkTheme
+                                    ? styles.subscribeButtonDark
+                                    : styles.subscribeButtonLight
+                            }>
+                            Subscribe
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div
-              className={
-                isDarkTheme
-                  ? styles.subscribeHeadingDark
-                  : styles.subscribeHeadingLight
-              }
-            >
-              Get our latest News and Docs in your INBOX.
-            </div>
-          </div>
-          <div>
-            <input
-              placeholder="Enter email address"
-              className={
-                isDarkTheme
-                  ? styles.subscribeInputDark
-                  : styles.subscribeInputLight
-              }
-            />
-            <button
-              className={
-                isDarkTheme
-                  ? styles.subscribeButtonDark
-                  : styles.subscribeButtonLight
-              }
-            >
-              Subscribe 
-            </button>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <>
-      <Layout
-        // title={`${siteConfig.title}`}
-        description="Description will go into a meta tag in <head />"
-      >
-        <HomepageHeader />
-        <HomepageFeatures />
-        <SubscribeNow />
-      </Layout>
-    </>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <>
+            <Layout
+                title="LIBONOMY Next Generation Of Blockchain"
+                description="Libonomy is Artificial Intelligence controlled blockchain that solves previously known issues like high energy usage, scalability, centralization, accessibility, interoperability and security. It is a completely autonomous multi-layer blockchain that strives evenly well on both private and public blockchains"
+                img="img/favicon.png">
+                <HomepageHeader />
+                <HomepageFeatures />
+                <SubscribeNow />
+            </Layout>
+        </>
+    );
 }
